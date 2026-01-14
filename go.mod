2d600544b0838775f33c703e8713de22cb20609d
anyongjin <anyongjin163@163.com>
2026-01-14 15:46:55 +0800

    support okx

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ toolchain go1.24.9
 
 //replace github.com/banbox/banbot => ../banbot
 
+//replace github.com/banbox/banexg => ../banexg
+
 require (
-	github.com/banbox/banbot v0.2.30
-	github.com/banbox/banexg v0.2.43
+	github.com/banbox/banbot v0.2.31
+	github.com/banbox/banexg v0.2.46
 	github.com/banbox/banta v0.3.5
 	go.uber.org/zap v1.27.0
 	gonum.org/v1/gonum v0.16.0
--- tmp/limit_order.go
+++ tmp/limit_order.go
@@ -2,32 +2,41 @@ package tmp
 
 import (
 	"github.com/banbox/banbot/config"
+	"github.com/banbox/banbot/orm/ormo"
 	"github.com/banbox/banbot/strat"
+	"github.com/banbox/banexg/log"
+	"go.uber.org/zap"
 )
 
 // LimitOrder 限价单策略：当没有订单时，按价格的90%创建限价单，3个bar未成交则取消
 func LimitOrder(pol *config.RunPolicyConfig) *strat.TradeStrat {
 	return &strat.TradeStrat{
-		WarmupNum: 10,
+		WarmupNum: 0,
 		OnBar: func(s *strat.StratJob) {
 			e := s.Env
 
 			// 检查是否有活跃订单，如果有直接返回
-			if len(s.LongOrders) > 0 || len(s.ShortOrders) > 0 {
+			if len(s.LongOrders) > 0 || len(s.ShortOrders) > 0 || s.IsWarmUp {
 				return
 			}
 
 			// 没有活跃订单时，创建限价单
 			// 获取当前价格的90%作为限价买入价格
 			currentPrice := e.Close.Get(0)
-			limitPrice := currentPrice * 0.997
+			limitPrice := currentPrice * 0.999
 
+			log.Info("trigger at", zap.Float64("price", limitPrice))
 			// 创建限价买入订单，使用StopBars设置3个bar后自动取消
 			s.OpenOrder(&strat.EnterReq{
-				Tag:      "limit_buy",
+				Tag: "test",
+				// Short: true,
+				// Stop:  limitPrice,
 				Limit:    limitPrice, // 限价单价格
 				StopBars: 3,          // 3个bar未成交则取消
 			})
+		},
+		OnOrderChange: func(s *strat.StratJob, od *ormo.InOutOrder, chgType int) {
+			log.Info("order change", zap.Int64("order_id", od.ID), zap.Int("chg_type", chgType))
 		},
 	}
 }
